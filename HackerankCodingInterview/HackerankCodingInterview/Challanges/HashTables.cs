13e6ac81967a8caea3450374e76c7296674bb5ce
AF <fimine@gmail.com>
2016-10-02 15:50:25 -0400

    Ransom note begin

---
+++ HackerankCodingInterview/HackerankCodingInterview/Challanges/HashTables.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerankCodingInterview.Challanges
+{
+	public static class HashTables
+	{
+		public static void RansomNote()
+		{
+			string[] tokens_m = Console.ReadLine().Split(' ');
+			int m = Convert.ToInt32(tokens_m[0]);
+			int n = Convert.ToInt32(tokens_m[1]);
+			string[] magazine = Console.ReadLine().Split(' ');
+			string[] ransom = Console.ReadLine().Split(' ');
+
+		}
+	}
+}
