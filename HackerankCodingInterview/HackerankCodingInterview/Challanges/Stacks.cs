ada289170b17e248d227b66e16a4d4b411d51746
U1 <U1@U1-PC>
2016-10-28 21:51:20 -0400

    brackets start

---
+++ HackerankCodingInterview/HackerankCodingInterview/Challanges/Stacks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerankCodingInterview.Challanges
+{
+	public static class Stacks
+	{
+		static void BalancedBrackets(String[] args)
+		{
+			int t = Convert.ToInt32(Console.ReadLine());
+			for (int a0 = 0; a0 < t; a0++)
+			{
+				string expression = Console.ReadLine();
+			}
+
+		}
+
+
+
+	}
+}
