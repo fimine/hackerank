384176ae489b0f850fd6d07c63dd8bf35aff1b0d
alexei.fimine <alexei.fimine@r3d.com>
2016-11-04 13:44:22 -0400

    wip

---
+++ HackerankCodingInterview/HackerankCodingInterview/Challanges/Algorythms.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerankCodingInterview.Challanges
+{
+	public static class Algorythms
+	{
+		static int c = 0;
+		
+		public static void CountingInversions(string[] args)
+		{
+
+
+
+			int t = Convert.ToInt32(Console.ReadLine());
+			for (int a0 = 0; a0 < t; a0++)
+			{
+				c = 0;
+				int n = Convert.ToInt32(Console.ReadLine());
+				string[] arr_temp = Console.ReadLine().Trim().Split(' ');
+				int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+				var arrTemp = new int[arr.Length];
+
+				MergeSort(arr, arrTemp, 0, arr.Length - 1);
+				Console.WriteLine(c);
+				
+			}
+
+
+
+		}
+
+		private static void MergeSort(int[] arr, int[] arrTemp, int start, int end)
+		{
+			if (start >= end)
+				return;
+
+			int mid = start / 2 + end / 2;
+
+			MergeSort(arr, arrTemp, start, mid);
+			MergeSort(arr, arrTemp, mid + 1, end);
+			MergeHalves(arr, arrTemp, start, end);
+
+		}
+
+		private static void MergeHalves(int[] arr, int[] arrTemp, int left, int end)
+		{
+			int leftEnd = left/2 + end/2;
+			int right = leftEnd + 1;
+			int i = left;
+			int length = end - left + 1;
+
+			while (left <= leftEnd && right <= end)
+			{
+				if (arr[left] > arr[right])
+				{
+					arrTemp[i] = arr[right];
+					left++;
+					c++;
+
+				}
+				else
+				{
+					if (arr[left] < arr[right])
+					{
+						//c++;
+						arrTemp[i] = arr[left];
+					}
+
+					
+					right ++;
+
+				}
+				i++;
+			}
+			
+			Array.Copy(arr, left, arrTemp, i,   leftEnd-left+1);
+			Array.Copy(arr, right, arrTemp, i, end - right + 1);
+			Array.Copy(arrTemp, arr, length);
+
+		}
+
+	}
+}
